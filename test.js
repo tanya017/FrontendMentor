8b43cad6cd7bc3f5c7a0394cc4280bf162745cb7
tanya <tanyaa0172004@gmail.com>
2024-11-10 13:46:29 +0530

    updated test.js

--- test.js
+++ test.js
@@ -1,14 +1,40 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+const server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+        // Serve the HTML file
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/style.css') {
+        // Serve the CSS file
+        fs.readFile(path.join(__dirname, 'public', 'style.css'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/css' });
+                res.end(data);
+            }
+        });
+    } else {
+        // Serve a 404 page if the file is not found
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 - Not Found');
+    }
 });
 
-server.listen(port, hostname, () =>{
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
